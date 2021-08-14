ca6068fea2bd2f02b5ac6d5cc59ab68679a618d3
fatpo <xihuanjianguo@gmail.com>
2021-08-14 21:26:04 +0800

    init kafka demo

---
+++ kakfa/src/main/java/ProducerDemo.java
@@ -0,0 +1,31 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static final String brokerList = "1.116.156.79:9092";
+    public static final String topic = "test2";
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("key.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer",
+                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("bootstrap.servers", brokerList);
+
+
+        System.out.println("send a message: hello, Kafka!");
+        KafkaProducer<String, String> producer =
+                new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record =
+                new ProducerRecord<>(topic, "hello, Kafka!");
+        try {
+            producer.send(record);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        producer.close();
+    }
+}
